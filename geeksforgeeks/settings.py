91c45bff6a5d20d2c6985c9192115a3780844aee
ayushbackpsn <ayushvaish.00005@gmail.com>
2025-06-13 21:39:23 +0530

    Update settings.py

--- geeksforgeeks/settings.py
+++ geeksforgeeks/settings.py
@@ -127,5 +127,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'blahfreee@gmail.com'
-EMAIL_HOST_PASSWORD = 'cjacnagvnwnxvxxb'
+EMAIL_HOST_USER = 'add your email'
+EMAIL_HOST_PASSWORD = 'add your app password'
